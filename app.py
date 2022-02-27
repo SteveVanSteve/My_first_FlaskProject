51e752d7cb25862cbbbe23c6e97b216b7ecde8a9
Steve Shields <stevie.shields@hotmail.co.uk>
2022-02-27 15:13:33 +0000

    Updates 003 (css + html + prepped for heroku)

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app.secret_key = "manbearpig_MUDMAN888"
 
 @app.route("/hello")
 def index():
-    flash("what's your name?")
+    flash("Hello there, what's your name?")
     return render_template("index.html")
 
 
 @app.route("/greet", methods=["POST", "GET"])
 def greeter():
-    flash("Hi " + str(request.form["name_input"]) + ", great to see you!")
+    flash("Hi " + str(request.form["name_input"]) +
+          ", it's great to see you!")
     return render_template("index.html")
